bda7d9243b44f20b345c8908caade213ed14efdf
Chris Cho <chris.cho@10gen.com>
2021-03-16 10:58:07 -0400

    monolight codec

---
+++ source/includes/fundamentals/code-snippets/MonolightCodec.java
@@ -0,0 +1,58 @@
+package fundamentals.monolightcodec;
+
+import org.bson.BsonReader;
+import org.bson.BsonType;
+import org.bson.BsonWriter;
+import org.bson.codecs.Codec;
+import org.bson.codecs.DecoderContext;
+import org.bson.codecs.EncoderContext;
+import org.bson.codecs.configuration.CodecRegistry;
+
+public class MonolightCodec implements Codec<Monolight>{
+
+    private CodecRegistry registry;
+    private Codec<String> powerStatusCodec; 
+    private Codec<Integer> integerCodec;
+    
+    public MonolightCodec(CodecRegistry registry) {
+        this.registry = registry;
+        this.integerCodec = registry.get(Integer.class);
+        this.powerStatusCodec = registry.get(String.class);
+        
+    }
+    
+    @Override
+    public void encode(BsonWriter writer, Monolight value, EncoderContext encoderContext) {
+        writer.writeStartDocument();
+        writer.writeName("powerStatus");
+        powerStatusCodec.encode(writer, value.getPowerStatus(), encoderContext);
+        writer.writeName("colorTemperature");
+        writer.writeInt32(value.getColorTemperature());
+        writer.writeEndDocument();
+    }
+
+    @Override
+    public Monolight decode(BsonReader reader, DecoderContext decoderContext) {
+        Monolight monolight = new Monolight();
+        
+        reader.readStartDocument();
+        while (reader.readBsonType() != BsonType.END_OF_DOCUMENT) {
+            String fieldName = reader.readName();
+            if (fieldName.equals("powerStatus")) {
+                monolight.setPowerStatus(powerStatusCodec.decode(reader, decoderContext));
+            } else if (fieldName.equals("colorTemperature")) {
+                monolight.setColorTemperature(reader.readInt32());
+            } else if (fieldName.equals("_id")){
+                reader.readObjectId(); 
+            }
+        }
+        reader.readEndDocument();
+        
+        return monolight;
+    }
+    @Override
+    public Class<Monolight> getEncoderClass() {
+        return Monolight.class;
+    }
+    
+}
