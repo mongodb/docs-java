b1d3131e9c7259eb54d8dbe2e1252c75765ff367
Kailie Yuan <kailie.yuan@10gen.com>
2021-04-06 14:39:05 -0400

    updated intro

--- source/includes/fundamentals/code-snippets/InsertUpdate.java
+++ source/includes/fundamentals/code-snippets/InsertUpdate.java
@@ -8,11 +8,9 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.ReplaceOptions;
 import com.mongodb.client.model.UpdateOptions;
 import com.mongodb.client.model.Updates;
 
@@ -71,26 +69,15 @@ public class InsertUpdate {
 
     private void setupPaintCollection() {
 
-        List<Document> deletedata = new ArrayList<>();
-
-        Document p1 = new Document("color", "red").append("qty", 5);
-        Document p2 = new Document("color", "purple").append("qty", 8);
-        Document p3 = new Document("color", "blue").append("qty", 0);
-        Document p4 = new Document("color", "white").append("qty", 0);
-        Document p5 = new Document("color", "yellow").append("qty", 6);
-        Document p6 = new Document("color", "pink").append("qty", 0);
-        Document p7 = new Document("color", "green").append("qty", 0);
-        Document p8 = new Document("color", "black").append("qty", 8);
-        
-        deletedata.add(p1);
-        deletedata.add(p2);
-        deletedata.add(p3);
-        deletedata.add(p4);
-        deletedata.add(p5);
-        deletedata.add(p6);
-        deletedata.add(p7);
-        deletedata.add(p8);
-
-        collection.insertMany(deletedata);
+        collection.insertMany(Arrays.asList(
+            new Document("color", "red").append("qty", 5), 
+            new Document("color", "purple").append("qty", 8), 
+            new Document("color", "blue").append("qty", 0), 
+            new Document("color", "white").append("qty", 0),
+            new Document("color", "yellow").append("qty", 6),
+            new Document("color", "pink").append("qty", 0),
+            new Document("color", "green").append("qty", 0),
+            new Document("color", "black").append("qty", 8)
+        ));
     }
 }
