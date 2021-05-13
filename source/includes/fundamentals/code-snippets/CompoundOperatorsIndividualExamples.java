4b58d412bd310e4a3e542d1b6a952931b98127a9
Aleksander Binion <alek.binion@mongodb.com>
2021-05-12 22:39:29 -0400

    add code examples for compound methods

--- source/includes/fundamentals/code-snippets/CompoundOperatorsIndividualExamples.java
+++ source/includes/fundamentals/code-snippets/CompoundOperatorsIndividualExamples.java
@@ -5,27 +5,35 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.*;
+import com.mongodb.client.result.InsertOneResult;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class CompoundOperatorsIndividualExamples {
 
-    public static final String COLLECTION = "compound-test-small-examples";
-    public static final String DATABASE = "test";
+    private static final String COLLECTION = "compound-ops";
+    private static final String DATABASE = "test";
 
     public static void main(String[] args) throws InterruptedException {
         CompoundOperatorsIndividualExamples.resetExample();
         CompoundOperatorsIndividualExamples examples = new CompoundOperatorsIndividualExamples();
-        examples.findAndUpdateExample();
+        examples.findOneAndUpdateExample();
+        examples.findOneAndReplaceExample();
+        examples.findOneAndDeleteExample();
     }
 
     public static void resetExample() {
-        MongoCollection<Document> collection = CompoundOperators.getCollection();
-        UpdateOptions options = new UpdateOptions().upsert(true);
-        Bson update = Updates.combine(Updates.set("food", "donut"), Updates.set("color", "green"));
-        collection.updateOne(new Document(), update, options);
+        MongoCollection<Document> collection = getCollection();
+        collection.deleteMany(new Document());
+        Document insert_pizza = new Document("_id", 1).append("food", "donut").append("color", "green");
+        Document insert_pear = new Document("_id", 2).append("food", "pear").append("color", "yellow");
+        ArrayList<Document> docs = new ArrayList<Document>();
+        docs.add(insert_pizza);
+        docs.add(insert_pear);
+        collection.insertMany(docs);
     }
 
     public static MongoCollection<Document> getCollection(){
@@ -36,7 +44,8 @@ public class CompoundOperatorsIndividualExamples {
         return collection;
     }
 
-    private void findAndUpdateExample(){
+    private void findOneAndUpdateExample(){
+        System.out.println("Starting find one and update example...");
         MongoCollection<Document> collection = getCollection();
         //start findOneAndUpdate-example
         // <MongoCollection set up code here>
@@ -47,10 +56,37 @@ public class CompoundOperatorsIndividualExamples {
                 projection(projection).
                 upsert(true).
                 maxTime(5, TimeUnit.SECONDS);
-
         Document result = collection.findOneAndUpdate(filter, update, options);
         System.out.println(result.toJson());
         //end findOneAndUpdate-example
     }
 
+    private void findOneAndDeleteExample(){
+        System.out.println("Starting find one and delete example...");
+        MongoCollection<Document> collection = getCollection();
+        //start findOneAndDelete-example
+        // <MongoCollection set up code here>
+        Bson sort = Sorts.descending("_id");
+        Document replace = new Document("music", "classical").append("color", "green");
+        FindOneAndDeleteOptions options = new FindOneAndDeleteOptions().
+                sort(sort);
+        Document result = collection.findOneAndDelete(new Document(), options);
+        System.out.println(result.toJson());
+        //end findOneAndDelete-example
+    }
+
+
+    private void findOneAndReplaceExample() {
+        System.out.println("Starting find one and replace example...");
+        MongoCollection<Document> collection = getCollection();
+        //start findOneAndReplace-example
+        // <MongoCollection set up code here>
+        Bson filter = Filters.eq("color", "green");
+        Document replace = new Document("music", "classical").append("color", "green");
+        FindOneAndReplaceOptions options = new FindOneAndReplaceOptions().
+                returnDocument(ReturnDocument.AFTER);
+        Document result = collection.findOneAndReplace(filter, replace, options);
+        System.out.println(result.toJson());
+        //end findOneAndReplace-example
+    }
 }
