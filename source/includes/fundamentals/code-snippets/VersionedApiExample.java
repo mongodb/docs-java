9d70dfe0326979900fa59f07eb269be4e5365725
Chris Cho <chris.cho@10gen.com>
2021-05-24 18:04:14 -0400

    add versioned api code

---
+++ source/includes/fundamentals/code-snippets/VersionedApiExample.java
@@ -0,0 +1,42 @@
+package fundamentals;
+
+import java.util.Arrays;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+public class VersionedAPIExample {
+
+    private static void setApiVersionOptions(String uri) {
+        // start apiVersionOptions
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .strict(true)
+                .deprecationErrors(true)
+                .build();
+        // end apiVersionOptions
+    }
+
+    public static void main(String[] args) {
+
+
+        // start serverAPIVersion
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .serverApi(serverApi)
+                .applyToClusterSettings(builder ->
+                        builder.hosts(Arrays.asList(new ServerAddress("localhost", 27017))))
+                .build();
+
+        MongoClient client = MongoClients.create(settings);
+        // end serverAPIVersion
+    }
+}
+
