85b159ca32185e2ab51d4639a4d36966fcdf2689
Aleksander Binion <alek.binion@mongodb.com>
2021-05-25 11:47:53 -0400

    add cluster monitor sample

--- source/includes/fundamentals/code-snippets/Monitoring.java
+++ source/includes/fundamentals/code-snippets/Monitoring.java
@@ -2,15 +2,13 @@ package com.mycompany.app;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
+import com.mongodb.ReadPreference;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.*;
-import com.mongodb.event.CommandFailedEvent;
-import com.mongodb.event.CommandListener;
-import com.mongodb.event.CommandStartedEvent;
-import com.mongodb.event.CommandSucceededEvent;
+import com.mongodb.event.*;
 import com.mongodb.management.JMXConnectionPoolListener;
 import org.bson.Document;
 import org.bson.conversions.Bson;
@@ -28,12 +26,13 @@ public class Monitoring {
 
     public static void main(String[] args) throws InterruptedException {
         Monitoring examples = new Monitoring();
+        System.out.println("\n---Command Event---\n");
         examples.monitorCommandEvent();
+        System.out.println("\n---Cluster Event---\n");
+        examples.monitorClusterEvent();
 
     }
-
-
-
+    
     private void monitorCommandEvent() {
         // start monitor-command-example
         MongoClientSettings settings =
@@ -49,6 +48,23 @@ public class Monitoring {
         collection.countDocuments();
         collection.distinct("music", Filters.empty(), String.class).forEach(i->{});
         // end monitor-command-example
+    }
+
+    private void monitorClusterEvent() {
+        // start monitor-cluster-example
+        MongoClientSettings settings =
+                MongoClientSettings.builder()
+                        .applyConnectionString(URI)
+                        .applyToClusterSettings(builder ->
+                                builder.addClusterListener(new TestClusterListener(ReadPreference.secondary())))
+                        .build();
+        MongoClient mongoClient = MongoClients.create(settings);
+        MongoDatabase database = mongoClient.getDatabase(DATABASE);
+        MongoCollection<Document> collection = database.getCollection(COLLECTION);
+        // Run a command to trigger a ClusterDescriptionChangedEvent event
+        collection.find().first().toJson();
+        mongoClient.close();
+        // end monitor-cluster-example
     }
 
 
@@ -87,3 +103,54 @@ class CommandTimer implements CommandListener {
     }
 }
 // end command-listener-impl
+
+// start cluster-listener-impl
+class TestClusterListener implements ClusterListener {
+    private final ReadPreference readPreference;
+    private boolean isWritable;
+    private boolean isReadable;
+
+    public TestClusterListener(final ReadPreference readPreference) {
+        this.readPreference = readPreference;
+    }
+
+    @Override
+    public void clusterOpening(final ClusterOpeningEvent clusterOpeningEvent) {
+        System.out.println(String.format("Cluster with unique client identifier %s opening",
+                clusterOpeningEvent.getClusterId().getValue()));
+    }
+
+    @Override
+    public void clusterClosed(final ClusterClosedEvent clusterClosedEvent) {
+        System.out.println(String.format("Cluster with unique client identifier %s closed",
+                clusterClosedEvent.getClusterId().getValue()));
+    }
+
+    @Override
+    public void clusterDescriptionChanged(final ClusterDescriptionChangedEvent event) {
+        if (!isWritable) {
+            if (event.getNewDescription().hasWritableServer()) {
+                isWritable = true;
+                System.out.println("Writable server available!");
+            }
+        } else {
+            if (!event.getNewDescription().hasWritableServer()) {
+                isWritable = false;
+                System.out.println("No writable server available!");
+            }
+        }
+
+        if (!isReadable) {
+            if (event.getNewDescription().hasReadableServer(readPreference)) {
+                isReadable = true;
+                System.out.println("Readable server available!");
+            }
+        } else {
+            if (!event.getNewDescription().hasReadableServer(readPreference)) {
+                isReadable = false;
+                System.out.println("No readable server available!");
+            }
+        }
+    }
+}
+// end cluster-listener-impl
