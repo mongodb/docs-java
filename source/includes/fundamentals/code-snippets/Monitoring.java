1b0f312433172a9ba4a322da0ac15a9e2797f442
Aleksander Binion <alek.binion@mongodb.com>
2021-05-25 23:24:23 -0400

    add connection pool example

--- source/includes/fundamentals/code-snippets/Monitoring.java
+++ source/includes/fundamentals/code-snippets/Monitoring.java
@@ -30,9 +30,10 @@ public class Monitoring {
         examples.monitorCommandEvent();
         System.out.println("\n---Cluster Event---\n");
         examples.monitorClusterEvent();
-
+        System.out.println("\n---Connection Pool Event---\n");
+        examples.monitorConnectionPoolEvent();
     }
-    
+
     private void monitorCommandEvent() {
         // start monitor-command-example
         MongoClientSettings settings =
@@ -48,15 +49,17 @@ public class Monitoring {
         collection.countDocuments();
         collection.distinct("music", Filters.empty(), String.class).forEach(i->{});
         // end monitor-command-example
+        mongoClient.close();
     }
 
     private void monitorClusterEvent() {
         // start monitor-cluster-example
+        TestClusterListener clusterListener = new TestClusterListener(ReadPreference.primary());
         MongoClientSettings settings =
                 MongoClientSettings.builder()
                         .applyConnectionString(URI)
                         .applyToClusterSettings(builder ->
-                                builder.addClusterListener(new TestClusterListener(ReadPreference.secondary())))
+                                builder.addClusterListener(clusterListener))
                         .build();
         MongoClient mongoClient = MongoClients.create(settings);
         MongoDatabase database = mongoClient.getDatabase(DATABASE);
@@ -67,7 +70,24 @@ public class Monitoring {
         // end monitor-cluster-example
     }
 
-
+    // start monitor-cp-example
+    private void monitorConnectionPoolEvent() {
+        // start monitor-cp-example
+        ConnectionPoolLibrarian cpListener = new ConnectionPoolLibrarian();
+        MongoClientSettings settings =
+                MongoClientSettings.builder()
+                        .applyConnectionString(URI)
+                        .applyToConnectionPoolSettings(builder ->
+                                builder.addConnectionPoolListener(cpListener))
+                        .build();
+        MongoClient mongoClient = MongoClients.create(settings);
+        MongoDatabase database = mongoClient.getDatabase(DATABASE);
+        MongoCollection<Document> collection = database.getCollection(COLLECTION);
+        // Run a command to trigger a connection pool events event
+        collection.find().first();
+        mongoClient.close();
+        // end monitor-cp-example
+    }
 
 }
 
@@ -128,10 +148,14 @@ class TestClusterListener implements ClusterListener {
 
     @Override
     public void clusterDescriptionChanged(final ClusterDescriptionChangedEvent event) {
+
+
         if (!isWritable) {
             if (event.getNewDescription().hasWritableServer()) {
                 isWritable = true;
                 System.out.println("Writable server available!");
+                event.getNewDescription()
+                        .getServerDescriptions().forEach(s -> System.out.println(s.getHosts()));
             }
         } else {
             if (!event.getNewDescription().hasWritableServer()) {
@@ -154,3 +178,15 @@ class TestClusterListener implements ClusterListener {
     }
 }
 // end cluster-listener-impl
+
+// start cp-listener-impl
+class ConnectionPoolLibrarian implements ConnectionPoolListener {
+
+    @Override
+    public void connectionCheckedOut(final ConnectionCheckedOutEvent event) {
+        System.out.println(String.format("Let me get you the connection with id %s...",
+                event.getConnectionId().getLocalValue()));
+    }
+
+}
+// end cp-listener-impl
