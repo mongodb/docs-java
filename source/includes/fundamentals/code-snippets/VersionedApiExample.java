2218529cf630be27c1d6612f3b864a7e1b7f2e4b
Chris Cho <chris.cho@10gen.com>
2021-06-02 12:54:30 -0400

    PR review fixes

--- source/includes/fundamentals/code-snippets/VersionedApiExample.java
+++ source/includes/fundamentals/code-snippets/VersionedApiExample.java
@@ -1,16 +1,13 @@
 package fundamentals;
 
-import java.util.Arrays;
-
+import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
-import com.mongodb.ServerAddress;
 import com.mongodb.ServerApi;
 import com.mongodb.ServerApiVersion;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 
-public class VersionedAPIExample {
-
+public class VersionedApiExample {
     private static void setApiVersionOptions(String uri) {
         // start apiVersionOptions
         ServerApi serverApi = ServerApi.builder()
@@ -27,15 +24,16 @@ public class VersionedAPIExample {
                 .version(ServerApiVersion.V1)
                 .build();
 
-        ServerAddress serverAddress = new ServerAddress("localhost", 27017);
+        // Replace the uri string with your MongoDB deployment's connection string
+        String uri = "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
 
         MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(uri))
                 .serverApi(serverApi)
-                .applyToClusterSettings(builder ->
-                        builder.hosts(Arrays.asList(serverAddress)))
                 .build();
 
         MongoClient client = MongoClients.create(settings);
         // end serverAPIVersion
     }
 }
+
