e1b618216e1e94ca510b72883d87eb48220566f2
Aleksander Binion <alek.binion@mongodb.com>
2021-06-03 15:19:30 -0400

    add current and legacy api code snippets

---
+++ source/includes/fundamentals/code-snippets/CurrentAPI.java
@@ -0,0 +1,31 @@
+package com.mycompany.app;
+
+
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.net.URL;
+
+public class CurrentAPI {
+
+    private static final String COLLECTION = "test";
+    private static final String DATABASE = "test";
+    private static final ConnectionString URI = new ConnectionString(System.getenv("DRIVER_URL"));
+
+    public static void main(String[] args) throws InterruptedException {
+        // start current-api-example
+        MongoClient client = MongoClients.create(URI);
+        MongoDatabase db = client.getDatabase(DATABASE);
+        MongoCollection col = db.getCollection(COLLECTION);
+        System.out.println(col.find().first().toString());
+        // end current-api-example
+
+    }
+
+
+
+}
---
+++ source/includes/fundamentals/code-snippets/LegacyAPI.java
@@ -0,0 +1,21 @@
+package com.mycompany.app;
+
+import com.mongodb.*;
+
+public class LegacyAPI {
+
+    private static final String COLLECTION = "test";
+    private static final String DATABASE = "test";
+    private static final ConnectionString URI = new ConnectionString(System.getenv("DRIVER_URL"));
+
+    public static void main(String[] args) {
+        // start legacy-api-example
+        MongoClient mongoClient = new MongoClient(URI);
+        DB db = mongoClient.getDB(DATABASE);
+        DBCollection col = db.getCollection(COLLECTION);
+        DBObject doc = col.find().one();
+        System.out.println(doc.toString());
+        // end legacy-api-example
+    }
+
+}
