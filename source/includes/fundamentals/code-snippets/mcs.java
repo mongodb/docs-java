d0b3c81b95a5a589f5483a27dfbda6b520c2a53f
Kailie Yuan <kailie.yuan@10gen.com>
2021-09-22 14:05:30 -0400

    reformatted intro

--- source/includes/fundamentals/code-snippets/mcs.java
+++ source/includes/fundamentals/code-snippets/mcs.java
@@ -14,12 +14,12 @@ import java.util.Collections;
 
 public class MCSettings {
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
 
         System.out.println("ConnectionString:");
         createViaConnectionString();
         System.out.println();
-        
+
         System.out.println("ClusterSettings:");
         createClusterSettings();
         System.out.println();
@@ -41,112 +41,106 @@ public class MCSettings {
         System.out.println();
     }
 
-    private static void createViaConnectionString(){
+    private static void createViaConnectionString() {
         try {
             //begin connectionString
             MongoClient mongoClient = MongoClients.create(
                 MongoClientSettings.builder()
-                        .applyConnectionString(new ConnectionString("<your connection uri>"))
-                        .build());
+                .applyConnectionString(new ConnectionString("<your connection uri>"))
+                .build());
             //end connectionString
             System.out.println(mongoClient.getClusterDescription());
             mongoClient.close();
-        }
-        finally {
+        } finally {
             System.out.println("---------------------------------------");
         }
-    } 
-    
-    private static void createClusterSettings(){
+    }
+
+    private static void createClusterSettings() {
         try {
             //begin clusterSettings
             MongoClient mongoClient = MongoClients.create(
                 MongoClientSettings.builder()
-                        .applyToClusterSettings(builder ->
-                                builder.serverSelectionTimeout(5, MILLISECONDS)
-                                        .mode(ClusterConnectionMode.LOAD_BALANCED)
-                                        .hosts(Collections.singletonList(new ServerAddress("host1", 27017))))
-                        .build());
+                .applyToClusterSettings(builder ->
+                    builder.serverSelectionTimeout(5, MILLISECONDS)
+                    .mode(ClusterConnectionMode.LOAD_BALANCED)
+                    .hosts(Collections.singletonList(new ServerAddress("host1", 27017))))
+                .build());
             //end clusterSettings
             System.out.println(mongoClient.getClusterDescription().getClusterSettings());
             mongoClient.close();
-        }
-        finally {
+        } finally {
             System.out.println("---------------------------------------");
         }
-    } 
+    }
 
-    private static void createServerSettings(){
-       try {
-           //begin ServerSettings
+    private static void createServerSettings() {
+        try {
+            //begin ServerSettings
             MongoClient mongoClient = MongoClients.create(
                 MongoClientSettings.builder()
-                        .applyToServerSettings(builder -> 
-                                builder.minHeartbeatFrequency(10, MILLISECONDS)
-                                        .heartbeatFrequency(3, SECONDS))
-                        .build());
+                .applyToServerSettings(builder ->
+                    builder.minHeartbeatFrequency(10, MILLISECONDS)
+                    .heartbeatFrequency(3, SECONDS))
+                .build());
             //end ServerSettings
             System.out.println(mongoClient.getClusterDescription().getServerSettings());
             mongoClient.close();
-       }
-       finally {
+        } finally {
             System.out.print("---------------------------------------");
         }
-    } 
+    }
 
-    private static void createSocketSettings(){
+    private static void createSocketSettings() {
         try {
             //begin SocketSettings
             MongoClient mongoClient = MongoClients.create(
                 MongoClientSettings.builder()
-                        .applyToSocketSettings(builder -> 
-                                builder.connectTimeout(1000, MILLISECONDS)
-                                        .readTimeout(10, SECONDS))
-                        .build());
+                .applyToSocketSettings(builder ->
+                    builder.connectTimeout(1000, MILLISECONDS)
+                    .readTimeout(10, SECONDS))
+                .build());
             //end SocketSettings
             System.out.println(mongoClient.getClusterDescription());
             mongoClient.close();
-        }
-        finally {
+        } finally {
             System.out.print("---------------------------------------");
         }
-     } 
+    }
 
-     private static void createConnectionPoolSettings(){
+    private static void createConnectionPoolSettings() {
         try {
             //begin ConnectionPoolSettings
             MongoClient mongoClient = MongoClients.create(
                 MongoClientSettings.builder()
-                        .applyToConnectionPoolSettings(builder -> 
-                                builder.maxWaitTime(5, SECONDS)
-                                        .maxSize(1)
-                                        .maxConnectionLifeTime(20, MILLISECONDS))
-                        .build());
+                .applyToConnectionPoolSettings(builder ->
+                    builder.maxWaitTime(5, SECONDS)
+                    .maxSize(1)
+                    .maxConnectionLifeTime(20, MILLISECONDS))
+                .build());
             //end ConnectionPoolSettings
             System.out.println(mongoClient.getClusterDescription());
             mongoClient.close();
-        }
-        finally {
+        } finally {
             System.out.print("---------------------------------------");
         }
-     } 
+    }
 
-     private static void createSslSettings(){
-        
+    private static void createSslSettings() {
+
         try {
             ////begin SslSettings
             MongoClient mongoClient = MongoClients.create(
                 MongoClientSettings.builder()
-                     .applyToSslSettings(builder -> 
-                            builder.enabled(true)
-                                    .invalidHostNameAllowed(false))
-                     .build());
+                .applyToSslSettings(builder ->
+                    builder.enabled(true)
+                    .invalidHostNameAllowed(false))
+                .build());
             //end SslSettings
             System.out.println(mongoClient.getClusterDescription());
             mongoClient.close();
-        }
-        finally {
+        } finally {
             System.out.print("---------------------------------------");
         }
-     }
+    }
 }
