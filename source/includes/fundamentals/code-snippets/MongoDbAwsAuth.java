e5d7b1377c98b9df97c4928f24fbd21f3b892ee5
Chris Cho <chris.cho@mongodb.com>
2023-08-10 17:15:07 -0400

    DOCSP-32129: fix ServerAddress placeholder for port (#426)
    (cherry picked from commit 830cdae8b1202b3f119ca15eb1a3f108fce1876e)

--- source/includes/fundamentals/code-snippets/MongoDbAwsAuth.java
+++ source/includes/fundamentals/code-snippets/MongoDbAwsAuth.java
@@ -41,7 +41,7 @@ public class MongoDbAwsAuth {
         MongoClient mongoClient = MongoClients.create(
                 MongoClientSettings.builder()
                 .applyToClusterSettings(builder ->
-                builder.hosts(Arrays.asList(new ServerAddress("<atlasUri>"))))
+                builder.hosts(Arrays.asList(new ServerAddress("<hostname>"))))
                 .credential(credential)
                 .build());
         // end mongoCredential
