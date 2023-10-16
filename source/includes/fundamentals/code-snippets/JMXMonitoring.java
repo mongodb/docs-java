069174b8353c7032cd78c26da8c114e237b786d9
norareidy <nora.reidy@mongodb.com>
2023-10-16 12:10:04 -0400

    more files

--- source/includes/fundamentals/code-snippets/JMXMonitoring.java
+++ source/includes/fundamentals/code-snippets/JMXMonitoring.java
@@ -12,14 +12,20 @@ public class JMXMonitoring {
     public static void main(String[] args) throws InterruptedException {
         // start jmx-example
         JMXConnectionPoolListener connectionPoolListener = new JMXConnectionPoolListener();
+        
         MongoClientSettings settings =
                 MongoClientSettings.builder()
                         .applyConnectionString(URI)
                         .applyToConnectionPoolSettings(builder -> builder.addConnectionPoolListener(connectionPoolListener))
                         .build();
+        
+        // Creates a MongoClient instance that enables connection pool event monitoring with the JMX tool
         MongoClient mongoClient = MongoClients.create(settings);
+        
         try {
             System.out.println("Navigate to JConsole to see your connection pools...");
+
+            // Pauses the code execution so you can navigate to JConsole and inspect your connection pools
             Thread.sleep(Long.MAX_VALUE);
         } catch (Exception e) {
             e.printStackTrace();
