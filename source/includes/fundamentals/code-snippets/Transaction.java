5ef670bdc4fff6ffa2d7027f46143f52d0b7272f
Stephanie Aurelio <stephanie.aurelio@mongodb.com>
2024-08-22 14:48:27 -0700

    update references

--- source/includes/fundamentals/code-snippets/Transaction.java
+++ source/includes/fundamentals/code-snippets/Transaction.java
@@ -1,9 +1,13 @@
-import com.mongodb.*;
+package fundamentals;
+
+// begin imports
 import com.mongodb.client.*;
+import com.mongodb.client.MongoClient;
 import com.mongodb.client.model.WriteConcern;
 import org.bson.Document;
 
 import java.util.Arrays;
+// end imports
 
 public class Transaction {
     public static void main(String[] args) {
