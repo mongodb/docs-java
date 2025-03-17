64bf34afc4ba7c05e0d404d55f1887e5a3cf8fd5
Rachel Mackintosh <rachel.mackintosh@mongodb.com>
2025-03-17 01:25:57 -0400

    tabs

--- source/includes/fundamentals/code-snippets/MCSettings.java
+++ source/includes/fundamentals/code-snippets/MCSettings.java
@@ -89,7 +89,8 @@ public class MCSettings {
         try {
             //begin SocketSettings
             MongoClient mongoClient = MongoClients.create(
-                MongoClientSettings.builder().applyConnectionString(new ConnectionString("<your connection string>"))
+                MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString("<your connection string>"))
                 .applyToSocketSettings(builder ->
                     builder.connectTimeout(10, SECONDS)
                     .readTimeout(15, SECONDS))
