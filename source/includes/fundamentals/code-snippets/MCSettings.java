c3abb134bda84d430ee37ab7410f19c1392c78a4
Rachel Mackintosh <rachel.mackintosh@mongodb.com>
2025-03-18 02:26:20 -0400

    mongo client settings

--- source/includes/fundamentals/code-snippets/MCSettings.java
+++ source/includes/fundamentals/code-snippets/MCSettings.java
@@ -141,6 +141,7 @@ public class MCSettings {
             ////begin LoggerSettings
             MongoClient mongoClient = MongoClients.create(
                 MongoClientSettings.builder().applyConnectionString(new ConnectionString("<your connection string>"))
+                .applicationName("<application name>")
                 .applyToLoggerSettings(builder ->
                      builder.maxDocumentLength(5_000))
                 .build());
